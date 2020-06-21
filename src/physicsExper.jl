30ca5253b0934b02ac64723a13a3c7e9b9b88c78
kn05 <jmegg10@gmail.com>
2020-06-22 04:03:47 +0900

    수정함:        src/physicsExper.jl

--- src/physicsExper.jl
+++ src/physicsExper.jl
@@ -2,9 +2,10 @@ using DifferentialEquations
 using Plots
 using CSV
 using FFTW
+using StatsBase
 
 filepostion = "/home/gnugnu/문서/Pendulum-experiment-analysis"
-filename = "num6"
+filename = "num4"
 data = CSV.read(filepostion*"/data/"*filename*".csv") 
 light = data[!,:2]  
 n=length(light)
@@ -15,17 +16,17 @@ for i in 1:n
 end
 
 l = 0.2                             # length [m]
-R = 0.012/2                             # radius of ball 
-m = 7.1206e-3                           # mass of ball 
-r = 0.007/2                             #radius of light sensor
+R = 0.012/2                         # radius of ball 
+m = 7.1206e-3                       # mass of ball 
+r = 0.007/2                         # radius of light sensor
 g = 9.81                            # gravitational acceleration [m/s²]
-t = 0.33
 dt = 2e-3
 
-θ₀ = atan(1/40)        # initial angular deflection [rad]
+θ₀ = atan(1/20)                    # initial angular deflection [rad]
 ω₀ = 0.0                           # initial angular velocity [rad/s]
 u₀ = [θ₀, ω₀]                       # initial state vector
-tspan = (0.0, t+data[n,:1])                 # time interval
+tspan = (0.0, data[n,:1])        # time interval
+tstop = 0.0:dt:data[n,:1]
 
 function f(theta) # i have to fix because ball and light's radius is different 
     a = l*sin(abs(theta/2))
@@ -43,13 +44,17 @@ function f2(theta)
   x2 = -sqrt(R^2-r^2) - a
 
   ans = 0;
-  if(x1<=-r) return s
+  if(x1<=-r) 
+    return s
   end
-  if(-r<=x1 && x1<=r) ans += (r-x1)/(2r)
+  if(-r<=x1 && x1<=r) 
+    ans += (r-x1)/(2r)
   end
-  if(-r<=x2 && x2<=r) ans += (r+x2)/(2r)
+  if(-r<=x2 && x2<=r) 
+    ans += (r+x2)/(2r)
   end
-  if(x2>=r) return s;
+  if(x2>=r) 
+    return s;
   end
   return ans*s
 end
@@ -76,9 +81,8 @@ for i in num
     for i in 1:length(sol.t)
         push!(result, f2(sol[1,i]))
     end
-    k = Int(t/dt)
     d= n
-    Fcal = fft(result[k+1:k+d])
+    Fcal = fft(result[1:d])
     absdeltaF = Float64[]
     for i in 1:length(Fexp)
         push!(absdeltaF, abs(Fexp[i]-Fcal[i]))
@@ -91,30 +95,25 @@ name = filepostion*"/result/"*filename*"/"*filename*"_AA.png"
 savefig(name)
 
 
-b = t -> 6*pi*1e-3*r+num[findfirst(x->x==minimum(AA),AA)] *1e-5
+b = t -> 6*pi*1e-3*r + num[findfirst(x->x==minimum(AA),AA)]*1e-5
 prob = ODEProblem(pendulum,u₀,tspan,b)
 alg = RK4()
-sol = solve(prob,alg,dt = 2e-3,adaptive=false)
+sol = solve(prob,alg,tstops = tstop ,adaptive=false)
 
 result = Float64[]
-for i in 1:length(sol.t)
+for i in 1:length(light)
     push!(result, f2(sol[1,i]))
 end
 
-k = Int(t/dt)
+c = crosscor(newlight, result)
+cmax = maximum(c)
+c = c/cmax
 
-start= 1
-d = 1000
-
-plot(sol.t[k+start:k+start+d], newlight[start:start+d], label = "data", size = (1920,1080), title = "simulation at b = "*string(b(0)))
-plot!(sol.t[k+start:k+start+d],result[k+start:k+start+d],label = "simulation")
-xlabel!("Time(s)")
-ylabel!("Light Intensity(%)")
-savefig(filepostion*"/result/"*filename*"/"*filename*"_sim_in_"*string(start*dt)*"to"*string((start+d)*dt)*".png")
-
-d= n
-plot(sol.t[k+1:k+d], newlight[1:d], label = "data", size = (1920,1080), title = "simulation at b = "*string(b(0)))
-plot!(sol.t[k+1:k+d],result[k+1:k+d],label = "simulation")
-xlabel!("Time(s)")
-ylabel!("Light Intensity(%)")
-savefig(filepostion*"/result/"*filename*"/"*filename*"_sim_in_whole.png")
+function graph(start, d)
+    t = findfirst(x->x==1.0, c)
+    plot(sol.t[start:start+d], newlight[start:start+d], label = "data", title = "simulation at b = "*string(b(0)))
+    plot!(sol.t[start:start+d],result[start-t:start+d-t],label = "simulation")
+    xlabel!("Time(s)")
+    ylabel!("Light Intensity(%)")
+    savefig(filepostion*"/result/"*filename*"/"*filename*"simulation("*string(start*dt)*", "*string((start+d)*dt)*").png")
+end
